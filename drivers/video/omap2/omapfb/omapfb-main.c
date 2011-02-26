49a083f642583cecf0da21056e17493c659132bb
Robert Nelson <robertcnelson@gmail.com>
2011-02-26 14:28:26 -0600

    Revert "OMAP: DSS2: OMAPFB: swap front and back porches for both hsync and vsync"
    This reverts commit 87ba8288670c53f66ce41d8ce292c64674de821e.
    
    Tweaked for v3.0
    
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>
    Signed-off-by: James Ang <angweiyang@gmail.com>

--- drivers/video/omap2/omapfb/omapfb-main.c
+++ drivers/video/omap2/omapfb/omapfb-main.c
@@ -722,10 +722,10 @@ int check_fb_var(struct fb_info *fbi, struct fb_var_screeninfo *var)
 		var->pixclock = timings.pixel_clock != 0 ?
 			KHZ2PICOS(timings.pixel_clock) :
 			0;
-		var->left_margin = timings.hbp;
-		var->right_margin = timings.hfp;
-		var->upper_margin = timings.vbp;
-		var->lower_margin = timings.vfp;
+		var->left_margin = timings.hfp;
+		var->right_margin = timings.hbp;
+		var->upper_margin = timings.vfp;
+		var->lower_margin = timings.vbp;
 		var->hsync_len = timings.hsw;
 		var->vsync_len = timings.vsw;
 	} else {
@@ -2048,10 +2048,10 @@ static int omapfb_mode_to_timings(const char *mode_str,
 	}
 
 	timings->pixel_clock = PICOS2KHZ(var->pixclock);
-	timings->hbp = var->left_margin;
-	timings->hfp = var->right_margin;
-	timings->vbp = var->upper_margin;
-	timings->vfp = var->lower_margin;
+	timings->hfp = var->left_margin;
+	timings->hbp = var->right_margin;
+	timings->vfp = var->upper_margin;
+	timings->vbp = var->lower_margin;
 	timings->hsw = var->hsync_len;
 	timings->vsw = var->vsync_len;
 	timings->x_res = var->xres;
